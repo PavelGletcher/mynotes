63b924553f9dc45f48d5419faef434b503ccec10
Павел Струначев <pavelstrunacev@iMac-Pavel.IGD_Rostelecom>
2023-07-31 15:37:45 +0500

    add start pack

---
+++ projectstartstructure/index.php
@@ -0,0 +1,31 @@
+<?php
+/* head */
+require 'head.php';
+?>
+  <title>Document</title>
+  <meta name="description" content="Описание">
+  <link rel="canonical" href="https://example.com">
+  
+  <meta name="keywords" content="Ключевики">
+  <meta name="author" content="Автор">
+  <meta name="image" content="./img/logo.png">
+
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Название">
+  <meta property="og:description" content="Описание">
+  <meta property="og:image" content="./img/logo.png">
+  <meta property="og:url" content="https://example.com">
+</head>
+<?php
+/* header */
+require 'header.html';
+?>
+<body>
+<div class="box--body container">
+  
+</div>
+</body>
+<?php
+/* footer */
+require 'footer.html';
+?>
